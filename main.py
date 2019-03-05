6bf212e87855a64245c3604a4e9997a50ae68e7c
Unknown <phuberts@gmail.com>
2019-03-05 17:22:39 +0100

    split into two files

--- main.py
+++ main.py
@@ -3,41 +3,7 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.app import App
 from kivy.clock import Clock
-import time
-import gphoto2 as gp
-import logging
-
-
-def cameradect():
-    logging.basicConfig(
-        format='%(levelname)s: %(name)s: %(message)s', level=logging.WARNING)
-    gp.check_result(gp.use_python_logging())
-    camera = gp.check_result(gp.gp_camera_new())
-    gp.check_result(gp.gp_camera_init(camera))
-    text = gp.check_result(gp.gp_camera_get_summary(camera))
-    print('Summary')
-    print('=======')
-    print(text.text)
-    print('Abilities')
-    print('=========')
-    abilities = gp.check_result(gp.gp_camera_get_abilities(camera))
-    print('model:', abilities.model)
-    print('status:', abilities.status)
-    print('port:', abilities.port)
-    print('speed:', abilities.speed)
-    print('operations:', abilities.operations)
-    print('file_operations:', abilities.file_operations)
-    print('folder_operations:', abilities.folder_operations)
-    print('usb_vendor:', abilities.usb_vendor)
-    print('usb_product:', abilities.usb_product)
-    print('usb_class:', abilities.usb_class)
-    print('usb_subclass:', abilities.usb_subclass)
-    print('usb_protocol:', abilities.usb_protocol)
-    print('library:', abilities.library)
-    print('id:', abilities.id)
-    print('device_type:', abilities.device_type)
-    gp.check_result(gp.gp_camera_exit(camera))
-    return 0
+import camera as cam
 
 
 class PhotoboothWidget(Widget):
@@ -56,7 +22,7 @@ class PhotoboothWidget(Widget):
 
         def count_it(count_from):
             if count_from == 1:
-                self.pic_preciew()
+                self.pic_preview()
                 return
             count_from -= 1
             self.count.text = str(count_from)
@@ -68,7 +34,7 @@ class PhotoboothWidget(Widget):
         self.remove_widget(self.count)
         self.add_widget(self.pic)
         self.pic.text = "photo"
-        cameradect()
+        cam.cameradect()
 
 
 class PhotoboothApp(App):
