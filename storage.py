94160b68da91642b4850c67ce1f56a1df61f92d4
Paul <p>
2019-03-09 14:33:54 +0100

    find pic with highest number

--- storage.py
+++ storage.py
@@ -23,17 +23,24 @@ def set_location(location):
     folder_location = location
     return
 
-
-def get_next_pic_name():
-    name = "pic000"
+def get_last_pic_name():
     list_of_files = os.listdir(get_location())
-
     def extract_number(f):
         s = re.findall("(\d+).jpg", f)
+        print(s)
         return (int(s[0]) if s else -1, f)
     if list_of_files:
-        name += (max(list_of_files, key=extract_number)) + ".jpg"
-        return name
+        print(list_of_files)
+        return max(list_of_files, key=extract_number)
     else:
-        name = "pic0000.jpg"
-        return name
+        return ""
+
+
+def get_next_pic_name():
+    max_file = get_last_pic_name()
+    if max_file:
+        max_file = max_file[3:-4]
+        max_file = int(max_file) + 1
+        return "pic" + str(max_file).zfill(4) + ".jpg"
+    else:
+        return "pic0000.jpg"
